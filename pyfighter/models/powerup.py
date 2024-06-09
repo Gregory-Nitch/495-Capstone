fec0d7504b12b848eef242e79bf65b85f98dafeb
Gregory Nitch <nitch.gregory@outlook.com>
2024-06-09 10:42:40 +0900

    Added type hints to PowerUp, light formating

--- pyfighter/models/powerup.py
+++ pyfighter/models/powerup.py
@@ -5,6 +5,8 @@ speed, or missile count when picked up.
 """
 
 import pygame
+from pygame import Vector2
+from pygame.mask import Mask
 from models.actor import Actor
 from models.player import Player
 from constants import SFX_PATHS
@@ -13,7 +15,15 @@ from constants import SFX_PATHS
 class PowerUp(Actor):
     """Power-up object for the game"""
 
-    def __init__(self, pos, speed, img, img_mask, offset, power_type):
+    def __init__(
+        self,
+        pos: Vector2,
+        speed: int,
+        img,
+        img_mask: Mask,
+        offset: dict,
+        power_type: str,
+    ):
         """Initializes a new instance of the PowerUp class"""
         super().__init__(pos, 0, speed, img, img_mask, offset)
         self.power_type = power_type
@@ -21,6 +31,7 @@ class PowerUp(Actor):
     def pickup(self, player: Player) -> None:
         """Applies the power-up effect to the player when picked up"""
 
+        # Only decrement if player cooldown is above 0 -> prevents negative cooldown
         if self.power_type == "fire_rate" and player.cooldown_threshold > 0:
             player.cooldown_threshold -= 2
         elif self.power_type == "speed":
@@ -28,14 +39,13 @@ class PowerUp(Actor):
         elif self.power_type == "missiles":
             player.missile_count += 1
 
-        # Play pickup sound
         self.play_pickup_sound()
 
-        # Display visual effect
         self.display_pickup_effect(player)
 
     def play_pickup_sound(self):
         """Plays a sound effect when the power-up is picked up."""
+
         pickup_sound = pygame.mixer.Sound(SFX_PATHS["powerup_pickup"])
         pickup_sound.play()
 
