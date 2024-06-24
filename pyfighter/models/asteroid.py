a957d7d40663821c436adcc62a84616ee2105940
mintycrisp <zfire107@gmail.com>
2024-06-24 17:31:51 +0900

    Added explosions for when asteroids are destroyed

--- pyfighter/models/asteroid.py
+++ pyfighter/models/asteroid.py
@@ -4,12 +4,16 @@ from pygame import mask
 from pygame import Vector2
 
 from models.actor import Actor
-
+from models.explosion import Explosion
 
 class Asteroid(Actor):
     """An asteroid to be randomly generated"""
 
-    def __init__(self, pos: Vector2, hp: int, speed: int, img, offset: dict):
+    def __init__(self, pos: Vector2, hp: int, speed: int, img, offset: dict, explosion_frames: list):
         super().__init__(pos, hp, speed, img, mask.from_surface(img), offset)
+        self.explosion_frames = explosion_frames
         # Above mask is created from the passed image because the img is
         # randomly chosen
+
+    def explode(self):
+        return Explosion(self.pos, self.explosion_frames)
---
+++ pyfighter/models/explosion.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame import Vector2
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, pos: Vector2, animation_frames: list):
+        super().__init__()
+        self.frames = animation_frames
+        self.current_frame = 0
+        self.image = self.frames[self.current_frame]
+        self.rect = self.image.get_rect(center=pos)
+        self.animation_counter = 0
+
+    def update(self):
+        if self.animation_counter % 5 == 0:  # Adjust this value to change the speed
+            self.current_frame += 1
+            if self.current_frame >= len(self.frames):
+                self.kill()  # Remove the explosion when animation is done
+            else:
+                self.image = self.frames[self.current_frame]
+        self.animation_counter += 1
